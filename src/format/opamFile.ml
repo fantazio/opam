6ffdee74b3124f0df72d2ec6677c193282b19bfc
Corentin De Souza <9597216+fantazio@users.noreply.github.com>
2026-05-11 15:40:40 +0200

    dead_code_analyzer:format/opamFile: cleanup unused values

--- src/format/opamFile.ml
+++ src/format/opamFile.ml
@@ -1738,7 +1738,6 @@ module InitConfigSyntax = struct
   let with_dl_tool dl_tool t = {t with dl_tool}
   let with_dl_jobs dl_jobs t = {t with dl_jobs}
   let with_dl_cache dl_cache t = {t with dl_cache = Some dl_cache}
-  let with_solver_criteria solver_criteria t = {t with solver_criteria}
   let with_solver solver t = {t with solver}
   let with_wrappers wrappers t = {t with wrappers}
   let with_global_variables global_variables t = {t with global_variables}
@@ -2296,8 +2295,6 @@ module Dot_configSyntax = struct
       (fun t -> None, Pp.print pp_contents t)
 
 
-  let variables t = List.rev_map fst t.vars
-
   let bindings t = t.vars
 
   let variable t s =
@@ -2365,7 +2362,6 @@ module RepoSyntax = struct
   let with_dl_cache dl_cache t = { t with dl_cache = Some dl_cache }
   let with_announce announce t = { t with announce }
   let with_stamp id t = { t with stamp = Some id }
-  let with_stamp_opt stamp t = { t with stamp }
 
   let fields = [
     "opam-version", Pp.ppacc_opt
@@ -2449,7 +2445,6 @@ module URLSyntax = struct
   let with_url url t = { t with url }
   let with_mirrors mirrors t = { t with mirrors }
   let with_checksum checksum t = { t with checksum = checksum }
-  let with_swhid swhid t = { t with swhid = Some swhid }
   let with_swhid_opt swhid t = { t with swhid = swhid }
   let with_subpath subpath t = { t with subpath = Some subpath }
   let with_subpath_opt subpath t = { t with subpath = subpath }
@@ -3964,11 +3959,6 @@ module CompSyntax = struct
     env       = [];
     tags      = [];
   }
-
-  let create_preinstalled name version packages env =
-    let mk n = Atom (n, Empty) in
-    let packages = OpamFormula.ands (List.map mk packages) in
-    { empty with name; version; preinstalled = true; packages; env }
 
   let name (t:t) = t.name
   let version (t:t) = t.version
